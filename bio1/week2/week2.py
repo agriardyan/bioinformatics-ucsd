ac7e0873e3c13de1cd4c9302a7832a2f7a0816b3
agriardyan <aagriard@gmail.com>
2022-07-09 21:15:34 +0700

    neighbor finding bio 1 week 2

--- bio1/week2/week2.py
+++ bio1/week2/week2.py
@@ -15,3 +15,49 @@ def hamming_distance(kmer1, kmer2):
         if(kmer1[i] != kmer2[i]):
             counter += 1
     return counter
+
+def immediate_neighbors(pattern):
+	"""
+	https://stepik.org/lesson/3014/step/1?unit=8228
+	"""
+	nucleotides = ['A', 'T', 'G', 'C']
+	neighborhoods = set()
+	for i,v in enumerate(pattern):
+		for _,n in enumerate(nucleotides):
+			if(n == v):
+				continue
+
+			neighbor = pattern[:i] + n + pattern[i+1:]
+			neighborhoods.add(neighbor)
+	
+	return neighborhoods
+
+def neighbors(pattern, d):
+	"""
+	https://stepik.org/lesson/3014/step/3?unit=8228
+	"""
+
+	if d == 0:
+		return set(pattern)
+	
+	nucleotides = {'A', 'T', 'G', 'C'}
+	if len(pattern) == 1:
+		return nucleotides	
+	
+	neighborhood = set()
+	suffix_neighbors = neighbors(pattern[1:], d)
+
+	for i,val in enumerate(suffix_neighbors):
+		if hamming_distance(pattern[1:], val) < d:
+			for _,n in enumerate(nucleotides):
+				neighborhood.add(n + val)
+			else:
+				neighborhood.add(pattern[0] + val)
+	
+	return neighborhood
+
+results = neighbors('CGAGACTTACTC', 3)
+for k,v in enumerate(results):
+	with open("test.txt", "a") as myfile:
+		myfile.write(v + ' ')
+	
